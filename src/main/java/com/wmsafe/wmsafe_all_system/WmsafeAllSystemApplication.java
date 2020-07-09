806cd651499aaa63e0a7dd895adba5a1116e2594
wmsafe <wmsafe@qq.com>
2020-07-09 16:32:48 +0800

    首次提交父pom.xml文件

---
+++ src/main/java/com/wmsafe/wmsafe_all_system/WmsafeAllSystemApplication.java
@@ -0,0 +1,13 @@
+package com.wmsafe.wmsafe_all_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WmsafeAllSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WmsafeAllSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wmsafe/wmsafe_all_system/WmsafeAllSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wmsafe.wmsafe_all_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WmsafeAllSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
