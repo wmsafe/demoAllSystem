8591ad82e136ebc965c018c1a0c7c795fb879fbf
17674600538 <hejingyu1363275177@outlook.com>
2020-07-09 20:40:15 +0800

    测试

--- src/main/java/com/wmsafe/wmsafe_all_system/WmsafeAllSystemApplication.java
+++ src/main/java/com/wmsafe/wmsafe_all_system/WmsafeAllSystemApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class WmsafeAllSystemApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(WmsafeAllSystemApplication.class, args);
     }
 
