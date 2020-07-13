984b61eef21db496fa60df692804d334d053f238
17674600538 <hejingyu1363275177@outlook.com>
2020-07-13 20:46:04 +0800

    更新module---2

--- firstproject/src/main/java/com/psfd/firstproject/controller/UserController.java
+++ firstproject/src/main/java/com/psfd/firstproject/controller/UserController.java
@@ -1,9 +1,12 @@
 package com.psfd.firstproject.controller;
 
 
+import com.psfd.firstproject.service.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * <p>
@@ -13,9 +16,20 @@ import org.springframework.stereotype.Controller;
  * @author Mht
  * @since 2020-07-12
  */
-@Controller
+@RestController
 @RequestMapping("/user")
 public class UserController {
 
+    @Autowired
+    private IUserService userServicel;
+
+    @RequestMapping("/login")
+    public ModelAndView login(String username,String password) {
+        System.out.println(username + "-------" + password);
+        userServicel.list();
+        ModelAndView andView = new ModelAndView();
+        andView.setViewName("index");
+        return andView;
+    }
 }
 
