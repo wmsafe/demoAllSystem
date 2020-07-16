687fac12e20868bc86a2330c1a10dfe55c1d4075
17674600538 <hejingyu1363275177@outlook.com>
2020-07-16 20:46:41 +0800

    更新module---7

--- firstproject/src/main/java/com/psfd/firstproject/controller/CommodityController.java
+++ firstproject/src/main/java/com/psfd/firstproject/controller/CommodityController.java
@@ -3,14 +3,11 @@ package com.psfd.firstproject.controller;
 
 import com.psfd.firstproject.entity.Commodity;
 import com.psfd.firstproject.service.ICommodityService;
-import org.apache.ibatis.annotations.Param;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
-
-import java.util.List;
 
 /**
  * <p>
